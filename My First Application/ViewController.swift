2722b93f1cb432ca32d081b49d555844f518c8db
SUP'Internet 07 <supinternet07@macbook-air-de-supinternet-72.local>
2016-10-19 12:05:12 +0200

    Initial Commit

---
+++ My First Application/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  My First Application
+//
+//  Created by SUP'Internet 07 on 19/10/2016.
+//  Copyright © 2016 François Mathieu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
