0445bbd3eb3e23cda170b1400cb79271a8d941c4
SUP'Internet 06 <supinternet06@macbook-air-de-supinternet-5.local>
2016-10-26 11:06:04 +0200

    Add textfield printing

--- My First Application/ViewController.swift
+++ My First Application/ViewController.swift
@@ -11,7 +11,8 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var myLabel: UILabel!
-    @IBOutlet weak var myTextField: UITextField!
+    @IBOutlet weak var loginField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
     @IBOutlet weak var myButton: UIButton!
     
     override func viewDidLoad() {
@@ -19,17 +20,25 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
         
         //First test
+        /*
         let view = UIView(frame: CGRect(x: 50, y: 50, width: 300, height: 300))
         view.backgroundColor = .blueColor()
         self.view.addSubview(view)
-        
+        */
         //Second test - label
         self.myLabel.text = "Hello Guys"
-        self.myTextField.text = "TextField"
+        self.loginField.text = "Login Field"
+        self.passwordField.text = "Password Field"
         self.myButton.setTitle("Click here !", forState: .Normal)
-
-
-
+    }
+    
+    @IBAction func useTouchedLoginButton() {
+        if let username = loginField.text,
+        let password = passwordField.text {
+            print("Username[\(username)]")
+            print("Password[\(password)]")
+        }
+        
     }
 
     override func didReceiveMemoryWarning() {
